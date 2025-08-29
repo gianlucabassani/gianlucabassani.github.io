ae9b4c69b0b40bdc2273a3b56f568ce339f09d07
SonicBoomGL <bassani.gianluca@gmail.com>
2025-08-29 15:37:05 +0200

    new package structure

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,18 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  // The 'base' property is not needed for a gh-pages site hosted at the root of a domain.
-  // Vite defaults to '/', which is the correct path.
+export default defineConfig({
+  base: '/',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development'
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
